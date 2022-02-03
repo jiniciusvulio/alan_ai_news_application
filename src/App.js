8a95473c54625293e8decd401ae534b1523b1224
jinicius vulio <jiniciusvulio@gmail.com>
2022-02-03 16:34:03 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import NewsCards from './components/NewsCards/NewsCards';
+
+const alanKey = '35c27c564e99722faa1088dee59ff6c52e956eca572e1d8b807a3e2338fdd0dc/stage';
+
+const App = () => {
+  const [ newsArticles, setNewsArticles ] = useState([]);
+
+  useEffect(()=>{
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command, articles }) => {
+        if(command === 'newHeadlines'){
+          setNewsArticles(articles);
+        }
+      }
+    })
+  }, []);
+
+  return(
+    <div>
+      <h1>Alan AI News Application</h1>
+      <NewsCards articles={newsArticles} />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/NewsCards/NewsCards.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import NewsCard from '../NewsCard/NewsCard';
+import { Grid, Grow, Typography } from '@material-ui/core'
+
+const NewsCards = ({ articles }) => {
+  return (
+    <Grow in>
+      <Grid container alignItems="stretch" spacing={3}>
+        {articles.map((articles, i)=>(
+          <Grid item xs={12} sm={6} md={4} lg={3} style={{display: 'flex'}}> 
+            <NewsCard />
+          </Grid>
+        ))}
+      </Grid>
+    </Grow>
+  )
+};
+
+export default NewsCards;
